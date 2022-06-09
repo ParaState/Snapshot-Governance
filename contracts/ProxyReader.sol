95413bd936d1d049a66f38169c29613a7791301d
jiangplus <jiang.plus.times@gmail.com>
2022-06-10 01:53:57 +0800

    use constant contract address

--- contracts/ProxyReader.sol
+++ contracts/ProxyReader.sol
@@ -12,13 +12,8 @@ abstract contract CompoundStakingReward {
 }
 
 contract ProxyReader {
-    address public stateTokenAddr;
-    address public stakingAddr;
-
-    constructor(address _stateTokenAddr, address _stakingAddr) {
-        stateTokenAddr = _stateTokenAddr;
-        stakingAddr = _stakingAddr;
-    }
+    address constant stateTokenAddr = 0xDA2F1D68d76DA861629C86b8823fE35807Cf8689;
+    address constant stakingAddr = 0x2Fcd20F97F27c3Ef78AC98189C3C1f22678A955E;
 
     function balanceOf(address _owner) public view returns (uint256) {
         return StateToken(stateTokenAddr).balanceOf(_owner) + 2 * CompoundStakingReward(stakingAddr).calculateSharesValueInState(_owner);
